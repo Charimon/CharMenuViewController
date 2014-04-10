58589008f4a563df893b249cec29ecad9865f3cf
Charimon <acharkin@gmail.com>
2014-04-09 23:32:39 -0700

    added proje

---
+++ CharMenuViewController/CharAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CharAppDelegate.h
+//  CharMenuViewController
+//
+//  Created by Andrew Charkin on 4/9/14.
+//  Copyright (c) 2014 Charimon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CharAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CharMenuViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CharMenuViewController
+//
+//  Created by Andrew Charkin on 4/9/14.
+//  Copyright (c) 2014 Charimon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CharAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CharAppDelegate class]));
+    }
+}
